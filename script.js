3132e664fc9204826afd15b3d05937648ff1d055
Jonibek <munirovjonibek@gmail.com>
2022-03-28 17:40:30 +0500

    added mark

--- script.js
+++ script.js
@@ -11,22 +11,46 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 let lat1, long1;
+let eventMap = 0;
+let map;
 navigator.geolocation.getCurrentPosition(function (e) {
   [lat1, long1] = [e.coords.latitude, e.coords.longitude];
-  var map = L.map('map').setView([lat1, long1], 20);
+  map = L.map('map').setView([lat1, long1], 20);
 
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution:
       '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
   }).addTo(map);
-
-  // L.marker([51.5, -0.09])
-  //   .addTo(map)
-  //   .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-  //   .openPopup();
+  L.marker([lat1, long1]).addTo(map).bindPopup('Siz shotta').openPopup();
+  map.on('click', function (e) {
+    eventMap = e;
+    form.classList.remove('hidden');
+    inputDistance.focus();
+    // let a = prompt('nima deb saqlaysiz');
+  });
 });
 
-setTimeout(function () {
-  console.log(`Lat ${lat1}  long ${long1}`);
-  console.log(`https://www.google.com/maps/@${lat1},${long1}z`);
-}, 3000);
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  inputDistance.value =
+    inputCadence.value =
+    inputDuration.value =
+    inputElevation.value =
+      '';
+  L.marker([eventMap.latlng.lat, eventMap.latlng.lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 2000,
+        minWidth: 20,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup',
+      }).setContent(`qayer`)
+    )
+    .openPopup();
+});
+inputType.addEventListener('change', function () {
+  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+});
